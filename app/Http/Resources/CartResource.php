089dca96f8a6776f240bfc10444d39c74ac2b83c
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-03-17 13:21:51 +0200

    feat: Add CartResource to provide a JSON representation of a cart, including items and calculated subtotal.

--- app/Http/Resources/CartResource.php
+++ app/Http/Resources/CartResource.php
@@ -14,16 +14,10 @@ class CartResource extends JsonResource
         // مجموع ما دُفع فعلاً
         $subTotal = $items->sum(fn($item) => $item->pivot->total_price);
 
-        $totalDiscount = $items->sum(function ($item) {
-            if ($item->pivot->product_size_id) return 0;
-            return max($item->price - $item->pivot->unit_price, 0) * $item->pivot->quantity;
-        });
-
         return [
             'id' => $this->id,
             'items' => CartItemResource::collection($this->whenLoaded('items')),
             'count' => $items->count(),
-            'total_discount' => round($totalDiscount, 2),
             'sub_total' => round($subTotal, 2),
         ];
     }
