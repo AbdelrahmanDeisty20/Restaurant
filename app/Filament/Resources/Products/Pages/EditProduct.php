49cef8384bac1f2ddedc3d01c6a90df0f07cc0e2
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-03-30 00:06:57 +0200

    feat: add View and Delete actions to EditProduct page and implement form data mutation for localized fields

--- app/Filament/Resources/Products/Pages/EditProduct.php
+++ app/Filament/Resources/Products/Pages/EditProduct.php
@@ -19,14 +19,14 @@ class EditProduct extends EditRecord
         ];
     }
 
-    // protected function mutateFormDataBeforeFill(array $data): array
-    // {
-    //     $data['name_ar'] = $this->record->name_ar;
-    //     $data['name_en'] = $this->record->name_en;
-    //     $data['description_ar'] = $this->record->description_ar;
-    //     $data['description_en'] = $this->record->description_en;
-    //     $data['main_image'] = $this->record->main_image;
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data['name_ar'] = $this->record->name_ar;
+        $data['name_en'] = $this->record->name_en;
+        $data['description_ar'] = $this->record->description_ar;
+        $data['description_en'] = $this->record->description_en;
+        $data['main_image'] = $this->record->main_image;
 
-    //     return $data;
-    // }
+        return $data;
+    }
 }
