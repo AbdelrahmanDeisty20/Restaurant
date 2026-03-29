a9e3e6b31c101be5d867922a90998b7992fd2565
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-03-29 23:54:24 +0200

    feat: add ViewProduct page with custom data mutation for product resource

--- app/Filament/Resources/Products/Pages/ViewProduct.php
+++ app/Filament/Resources/Products/Pages/ViewProduct.php
@@ -23,7 +23,7 @@ class ViewProduct extends ViewRecord
         $data['name_en'] = $this->record->name_en;
         $data['description_ar'] = $this->record->description_ar;
         $data['description_en'] = $this->record->description_en;
-        $data['image_path'] = $this->record->main_image;
+        $data['image_path'] = $this->record->image_path;
 
         return $data;
     }
