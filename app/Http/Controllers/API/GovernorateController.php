abaa76c5ad574e7b5fb3626065773ac687e8a277
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-03-31 11:15:03 +0200

    feat: add GovernorateController to handle governorate retrieval via API

--- app/Http/Controllers/API/GovernorateController.php
+++ app/Http/Controllers/API/GovernorateController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Services\GovernorateService;
 use App\Traits\ApiResponse;
-use Illuminate\Http\Request;
 
 class GovernorateController extends Controller
 {
