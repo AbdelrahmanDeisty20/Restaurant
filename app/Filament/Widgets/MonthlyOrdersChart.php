98036060bd34312a01192cf6a3c4074968789e0a
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-05 23:05:42 +0200

    feat: add MonthlyOrdersChart widget to display 12-month order trends

--- app/Filament/Widgets/MonthlyOrdersChart.php
+++ app/Filament/Widgets/MonthlyOrdersChart.php
@@ -14,9 +14,9 @@ class MonthlyOrdersChart extends ChartWidget
         return __('Monthly Orders Trend');
     }
 
-    protected static ?int $sort = 3;
+    protected static ?int $sort = 4;
 
-    protected int | string | array $columnSpan = 'full';
+    protected int | string | array $columnSpan = 1;
 
     protected function getData(): array
     {
