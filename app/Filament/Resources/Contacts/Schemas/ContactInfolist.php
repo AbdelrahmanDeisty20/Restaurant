2bdff662e48738261370a91dc21ffda7fcb77c88
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-06 13:23:35 +0200

    feat: add ContactInfolist schema for displaying contact details and messages

--- app/Filament/Resources/Contacts/Schemas/ContactInfolist.php
+++ app/Filament/Resources/Contacts/Schemas/ContactInfolist.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Contacts\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Schema;
 
 class ContactInfolist
@@ -11,15 +13,44 @@ class ContactInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('name'),
-                TextEntry::make('email')
-                    ->label(__('Email address')),
-                TextEntry::make('phone'),
-                TextEntry::make('subject'),
-                TextEntry::make('created_at')
-                    ->dateTime(),
-                TextEntry::make('updated_at')
-                    ->dateTime(),
+                Section::make(__('Contact Information'))
+                    ->icon('heroicon-o-user')
+                    ->description(__('Basic details of the person who contacted us.'))
+                    ->schema([
+                        Grid::make(3)
+                            ->schema([
+                                TextEntry::make('name')
+                                    ->label(__('Name'))
+                                    ->icon('heroicon-m-user'),
+                                TextEntry::make('email')
+                                    ->label(__('Email address'))
+                                    ->icon('heroicon-m-envelope')
+                                    ->copyable(),
+                                TextEntry::make('phone')
+                                    ->label(__('Phone'))
+                                    ->icon('heroicon-m-phone')
+                                    ->copyable(),
+                            ]),
+                    ]),
+
+                Section::make(__('Message Content'))
+                    ->icon('heroicon-o-chat-bubble-left-right')
+                    ->description(__('The specific details and message received.'))
+                    ->schema([
+                        TextEntry::make('subject')
+                            ->label(__('Subject'))
+                            ->weight('bold')
+                            ->color('primary'),
+                        TextEntry::make('message')
+                            ->label(__('Message'))
+                            ->prose()
+                            ->columnSpanFull()
+                            ->alignJustify(),
+                        TextEntry::make('created_at')
+                            ->label(__('Sent At'))
+                            ->dateTime()
+                            ->color('gray'),
+                    ]),
             ]);
     }
 }
